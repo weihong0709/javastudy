aef65a72cc617c9df0566812ee556ee629502ad7
liuweihong <liu.weihong1@zte.com.cn>
2020-03-19 08:53:01 +0800

    add kata

--- kata/src/main/java/com/erci/FindExpression.java
+++ kata/src/main/java/com/erci/FindExpression.java
@@ -2,54 +2,69 @@ package com.erci;
 
 public class FindExpression {
     public String findExpression(int i, int v) {
-        if (i == v) {
-            return String.valueOf(i);
-        }
         if (v < i) {
             return handWhenILessThanV(i, v);
         }
         return handWhenIMoreThanV(i, v);
-
     }
 
     public String handWhenILessThanV(int i, int v) {
-        if (v > i / 2) {
-            StringBuilder expression = new StringBuilder("" + i);
-            for (int j = 0; j < (i - v); j++) {
-                expression.append("-").append(i).append("/").append(i);
-            }
-            return expression.toString();
+        if (isVMoreThanIHalf(i, v)) {
+            return getDivisionExpression(i, v);
         }
+        return getAddExpression(i, v);
 
+    }
+
+    private String getDivisionExpression(int i, int v) {
+        StringBuilder expression = new StringBuilder("" + i);
+        for (int j = 0; j < (i - v); j++) {
+            expression.append("-").append(i).append("/").append(i);
+        }
+        return expression.toString();
+    }
+
+    private String getAddExpression(int i, int v) {
         StringBuilder expression = new StringBuilder();
         for (int j = 0; j < v; j++) {
             expression.append(i).append("/").append(i).append("+");
         }
         return expression.substring(0, expression.length() - 1).toString();
+    }
 
+
+    private boolean isVMoreThanIHalf(int i, int v) {
+        return v > i / 2;
     }
 
     public String handWhenIMoreThanV(int i, int v) {
         if (v % i == 0) {
-            for (int j = 0; j < v; j++) {
-                int result = (int) Math.pow(i, j);
-                if (result == v) {
-                    return getMultipyExpression(i, j);
+            return handWhenDivisible(i, v);
+        }
+        return handWhenNotDivisible(i, v);
+    }
 
-                } else if (result > v) {
-                    return getMultipyExpression(i, j - 1) + "+" + handWhenIMoreThanV(i, v - (int) Math.pow(i, j - 1));
-                }
+    private String handWhenNotDivisible(int i, int v) {
+        int left = v % i;
+        return handWhenIMoreThanV(i, v - left) + "+" + handWhenILessThanV(i, left);
+    }
+
+    private String handWhenDivisible(int i, int v) {
+        for (int j = 0; j < v; j++) {
+            int result = (int) Math.pow(i, j);
+            if (result == v) {
+                return getMultiplyExpression(i, j);
+
+            } else if (result > v) {
+                return getMultiplyExpression(i, j - 1) + "+" + handWhenIMoreThanV(i, v - (int) Math.pow(i, j - 1));
             }
-        } else {
-            int temp = v % i;
-            return handWhenIMoreThanV(i, v - temp)+"+"+ handWhenILessThanV(i, temp);
         }
         return null;
     }
 
-    private String getMultipyExpression(int i, int j) {
-        StringBuilder expression = new StringBuilder();
-        expression.append(i);
+
+    private String getMultiplyExpression(int i, int j) {
+        StringBuilder expression = new StringBuilder("" + i);
         for (int m = 2; m <= j; m++) {
             expression.append("*").append(i);
         }
--- kata/src/test/java/com/erci/FindExpressionTest.java
+++ kata/src/test/java/com/erci/FindExpressionTest.java
@@ -7,78 +7,87 @@ import static org.junit.Assert.assertEquals;
 
 public class FindExpressionTest {
     private static FindExpression findExpression;
+
     @BeforeClass
-    public static void init(){
+    public static void init() {
         findExpression = new FindExpression();
     }
+
     @Test
     public void testWhenVEqualsI() {
-        assertEquals("5",findExpression.findExpression(5,5));
+        assertEquals("5", findExpression.findExpression(5, 5));
 
     }
 
     /**
-     * i为偶数，且v大于i/2
+     * i为偶数，V小于I，且v大于i/2
      */
     @Test
     public void testWhenVLessThan1() {
-        assertEquals("6-6/6",findExpression.findExpression(6,5));
+        assertEquals("6-6/6", findExpression.findExpression(6, 5));
 
     }
+
     /**
-     * i为偶数，且v等于i/2
+     * i为偶数，V小于I，且v等于i/2
      */
     @Test
     public void testWhenVLessThanI2() {
-        assertEquals("6/6+6/6+6/6",findExpression.findExpression(6,3));
+        assertEquals("6/6+6/6+6/6", findExpression.findExpression(6, 3));
 
     }
+
     /**
      * i为偶数，且v小于i/2
      */
     @Test
     public void testWhenVLessThanI3() {
-        assertEquals("6/6+6/6",findExpression.findExpression(6,2));
+        assertEquals("6/6+6/6", findExpression.findExpression(6, 2));
 
     }
+
     /**
-     * i为偶数，且v等于1
+     * i为偶数，且v等于I
      */
     @Test
     public void testWhenVLessThanI4() {
-        assertEquals("6/6",findExpression.findExpression(6,1));
+        assertEquals("6/6", findExpression.findExpression(6, 1));
 
     }
+
     /**
-     * i为奇数，且v大于i/2取整
+     * i为奇数，且v大于i/2取整，但小于I
      */
     @Test
     public void testWhenVLessThanI5() {
-        assertEquals("5-5/5-5/5",findExpression.findExpression(5,3));
+        assertEquals("5-5/5-5/5", findExpression.findExpression(5, 3));
 
     }
+
     /**
      * i为奇数，且v等于i/2取整
      */
     @Test
     public void testWhenVLessThanI6() {
-        assertEquals("5/5+5/5",findExpression.findExpression(5,2));
+        assertEquals("5/5+5/5", findExpression.findExpression(5, 2));
 
     }
+
     /**
      * i为奇数，且v小于i/2取整
      */
-        @Test
-        public void testWhenVLessThanI7() {
-            assertEquals("7/7+7/7",findExpression.findExpression(7,2));
+    @Test
+    public void testWhenVLessThanI7() {
+        assertEquals("7/7+7/7", findExpression.findExpression(7, 2));
 
     }
+
     /**
-     * i为奇数，且v等于1
+     * i为奇数，且v等于I
      */
     @Test
     public void testWhenVLessThanI8() {
-        assertEquals("5/5",findExpression.findExpression(5,1));
+        assertEquals("5", findExpression.findExpression(5, 5));
 
     }
 
@@ -87,7 +96,7 @@ public class FindExpressionTest {
      */
     @Test
     public void testWhenVMoreThanI1() {
-        assertEquals("3*3",findExpression.findExpression(3,9));
+        assertEquals("3*3", findExpression.findExpression(3, 9));
 
     }
 
@@ -96,24 +105,43 @@ public class FindExpressionTest {
      */
     @Test
     public void testWhenVMoreThanI2() {
-        assertEquals("3*3*3+3*3",findExpression.findExpression(3,36));
+        assertEquals("3*3*3+3*3", findExpression.findExpression(3, 36));
 
     }
 
     /**
-     * V大于I，且v不等于I的整数倍
+     * V大于I，且v不等于I的整数倍，剩余的余数小于I的二分之一
      */
     @Test
     public void testWhenVMoreThanI3() {
-        assertEquals("3*3*3+3*3+3/3",findExpression.findExpression(3,37));
+        assertEquals("3*3*3+3*3+3/3", findExpression.findExpression(3, 37));
 
     }
+
+    /**
+     * V大于I，且v不等于I的整数倍，剩余的余数大于I的二分之一
+     */
+    @Test
+    public void testWhenVMoreThan4() {
+        assertEquals("3*3*3+3*3+3-3/3", findExpression.findExpression(3, 38));
+
+    }
+
+    /**
+     * V大于I，且v不等于I的整数倍，小于I的2倍
+     */
+    @Test
+    public void testWhenVMoreThanI5() {
+        assertEquals("3+3/3", findExpression.findExpression(3, 4));
+
+    }
+
     /**
      * V大于I，且v不等于I的整数倍
      */
     @Test
-    public void testWhenVMoreThanI4() {
-        assertEquals("3+3/3",findExpression.findExpression(3,4));
+    public void testWhenVMoreThanI6() {
+        assertEquals("7*7+7*7+7/7", findExpression.findExpression(7, 99));
 
     }
 
