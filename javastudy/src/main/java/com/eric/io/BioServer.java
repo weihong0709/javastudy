5d8ad4f7bcdeb1286b4befd9d85b30d6d62551fb
liuweihong <liu.weihong1@zte.com.cn>
2020-07-11 10:47:44 +0800

    javaIO修改

--- javastudy/src/main/java/com/eric/io/BioServer.java
+++ javastudy/src/main/java/com/eric/io/BioServer.java
@@ -2,6 +2,7 @@ package com.eric.io;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketAddress;
@@ -10,16 +11,34 @@ public class BioServer {
     public static void server(){
         ServerSocket serverSocket = null;
         InputStream in = null;
+        OutputStream out = null;
         try
         {
-            serverSocket = new ServerSocket(8080);
+            //创建Serversocket，可以设置ip地址、端口和backlog参数
+            serverSocket = new ServerSocket(8080,50);
+            //设置接收缓冲区的大小
+            serverSocket.setReceiveBufferSize(100);
             int recvMsgSize = 0;
             byte[] recvBuf = new byte[1024];
             while(true){
                 Socket clntSocket = serverSocket.accept();
+                //设置发送缓冲区的大小
+                clntSocket.setSendBufferSize(10);
+                clntSocket.setReceiveBufferSize(10);
+                //连接检测
+
+                clntSocket.setKeepAlive(true);
+                clntSocket.setOOBInline();
+                clntSocket.setReuseAddress();
+                clntSocket.setSoLinger(true,10);
+                clntSocket.setSoTimeout();
+                clntSocket.setTcpNoDelay();
+                clntSocket.setPerformancePreferences();
+                clntSocket.setTrafficClass();
                 SocketAddress clientAddress = clntSocket.getRemoteSocketAddress();
                 System.out.println("Handling client at "+clientAddress);
                 in = clntSocket.getInputStream();
+                out = clntSocket.getOutputStream();
                 while((recvMsgSize=in.read(recvBuf))!=-1){
                     byte[] temp = new byte[recvMsgSize];
                     System.arraycopy(recvBuf, 0, temp, 0, recvMsgSize);
--- javastudy/src/main/java/com/eric/io/NIOServer.java
+++ javastudy/src/main/java/com/eric/io/NIOServer.java
@@ -3,100 +3,45 @@ package com.eric.io;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
-import java.nio.channels.SelectionKey;
-import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
-import java.util.Iterator;
 
+
+/**
+ * 可以实现阻塞和非阻塞的IO
+ */
 public class NIOServer {
     private static final int BUF_SIZE=1024;
     private static final int PORT = 8080;
-    private static final int TIMEOUT = 3000;
     public static void main(String[] args)
     {
-        selector();
-    }
-    public static void handleAccept(SelectionKey key) throws IOException {
-        ServerSocketChannel ssChannel = (ServerSocketChannel)key.channel();
-        SocketChannel sc = ssChannel.accept();
-        sc.configureBlocking(false);
-        sc.register(key.selector(), SelectionKey.OP_READ, ByteBuffer.allocateDirect(BUF_SIZE));
-    }
-    public static void handleRead(SelectionKey key) throws IOException{
-        SocketChannel sc = (SocketChannel)key.channel();
-        ByteBuffer buf = (ByteBuffer)key.attachment();
-        long bytesRead = sc.read(buf);
-        while(bytesRead>0){
-            buf.flip();
-            while(buf.hasRemaining()){
-                System.out.print((char)buf.get());
-            }
-            System.out.println();
-            buf.clear();
-            bytesRead = sc.read(buf);
-        }
-        if(bytesRead == -1){
-            sc.close();
-        }
-    }
-    public static void handleWrite(SelectionKey key) throws IOException{
-        ByteBuffer buf = (ByteBuffer)key.attachment();
-        buf.flip();
-        SocketChannel sc = (SocketChannel) key.channel();
-        while(buf.hasRemaining()){
-            sc.write(buf);
-        }
-        buf.compact();
-    }
-
-    /**
-     * 四类事件，每种都表示该事件就绪，例如连接就绪、写就绪？
-     * 1. SelectionKey.OP_CONNECT
-     * 2. SelectionKey.OP_ACCEPT
-     * 3. SelectionKey.OP_READ
-     * 4. SelectionKey.OP_WRITE
-     */
-    public static void selector() {
-        Selector selector = null;
         ServerSocketChannel ssc = null;
         try{
-            selector = Selector.open();
+            //创建channel
             ssc= ServerSocketChannel.open();
-            ssc.socket().bind(new InetSocketAddress(PORT));
-            ssc.configureBlocking(false);
-            ssc.register(selector, SelectionKey.OP_ACCEPT);
+            //给channel对应的socket绑定IP地址,通过bind操作完成底层的bind和listen方法的调用
+            //可以设置backlog，也可以不设置backlog，不设置，默认值为50
+            ssc.socket().bind(new InetSocketAddress(PORT),100);
+            //配置为非阻塞
+            ssc.configureBlocking(true);
+            //将channel注册到select上面
+            SocketChannel socketChannel =  ssc.accept();
+            if (socketChannel != null) {
+                socketChannel.configureBlocking(true);
+                ByteBuffer byteBuffer = ByteBuffer.allocate(BUF_SIZE);
+                int readCount = socketChannel.read(byteBuffer);
+                byteBuffer.flip();
 
-            while(true){
-                if(selector.select(TIMEOUT) == 0){
-                    System.out.println("==");
-                    continue;
-                }
-                Iterator<SelectionKey> iter = selector.selectedKeys().iterator();
-                while(iter.hasNext()){
-                    SelectionKey key = iter.next();
-                    if(key.isAcceptable()){
-                        handleAccept(key);
-                    }
-                    if(key.isReadable()){
-                        handleRead(key);
-                    }
-                    if(key.isWritable() && key.isValid()){
-                        handleWrite(key);
-                    }
-                    if(key.isConnectable()){
-                        System.out.println("isConnectable = true");
-                    }
-                    iter.remove();
-                }
+                System.out.println("readCount:"+readCount);
+                System.out.println(byteBuffer.get());
+                System.out.println(byteBuffer.toString());
+
             }
+            System.out.println("success");
         }catch(IOException e){
             e.printStackTrace();
         }finally{
             try{
-                if(selector!=null){
-                    selector.close();
-                }
                 if(ssc!=null){
                     ssc.close();
                 }
